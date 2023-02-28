04d6cd09703dbda684990c93fbaeb282c571e67e
Leônidas <leonidas19992011@hotmail.com>
2023-02-28 09:35:29 -0300

    angular v15, inicio crud, delete adicionado

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//Interfaces
+import { TaskList } from './../../pages/model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+  public taskList: Array<TaskList> = [
+
+  ];
+
+  constructor() {}
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1)
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public deleteAllTaskList()
+  {
+    const confirm = window.confirm("Você deseja realmente Deletar tudo?");
+    if(confirm){
+    this.taskList = [];
+    }
+  }
 
 }
